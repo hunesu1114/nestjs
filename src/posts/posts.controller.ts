0d420db6f0de8c2770f7dc006d89bfcbd85cb002
hunesu Kim <kewcxea@hanmail.net>
2024-01-11 09:35:14 +0900

    240111

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,33 +1,43 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-/**
- * author : string;
- * title : string;
- * content : string;
- * likeCount : number;
- * commentCount : number;
- */
-interface Post{
-  author : string;
-  title : string;
-  content : string;
-  likeCount : number;
-  commentCount : number;
-}
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 100000,
-      commentCount: 99999,
-    };
+  getPosts() {
+    return this.postsService.getAllPosts();
   }
+
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return this.postsService.getPostById(+id);
+  }
+
+  @Post()
+  postPosts(
+      @Body('author') author: string,
+      @Body('title') title: string,
+      @Body('content') content: string,
+  ) {
+    return this.postsService.createPost(author, title, content);
+  }
+
+  @Patch(':id')
+  putPost(
+      @Param('id') id: string,
+      @Body('author') author?: string,
+      @Body('title') title?: string,
+      @Body('content') content?: string,) {
+    return this.postsService.updatePost(+id, author, title, content);
+  }
+
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(+id);
+  }
+
+
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,111 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+
+/**
+ * author : string;
+ * title : string;
+ * content : string;
+ * likeCount : number;
+ * commentCount : number;
+ */
+export interface PostModel{
+    id: number;
+    author : string;
+    title : string;
+    content : string;
+    likeCount : number;
+    commentCount : number;
+}
+
+let posts:PostModel[]=[
+    {
+        id: 1,
+        author: 'newjeans_official',
+        title: '뉴진스 민지',
+        content: '메이크업 고치고 있는 민지',
+        likeCount: 1000000,
+        commentCount: 999999,
+    },
+    {
+        id: 2,
+        author: 'newjeans_official',
+        title: '뉴진스 해린',
+        content: '노래 연습 하고 있는 해린',
+        likeCount: 1000000,
+        commentCount: 999999,
+    },
+    {
+        id: 3,
+        author: 'blackpink_official',
+        title: '블랙핑크 로제',
+        content: '종합운동장에서 공연중인 로제',
+        likeCount: 1000000,
+        commentCount: 999999,
+    },
+]
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    getAllPosts() {
+        return posts;
+    }
+
+    getPostById(id: number) {
+        const post = posts.find((post) => post.id === +id);
+
+        if (!post) {
+            throw new NotFoundException();
+        }
+
+        return post;
+    }
+
+    createPost(author: string, title: string, content: string) {
+        const post={
+            id: posts.length+1,
+            author: author,
+            title: title,
+            content: content,
+            likeCount: 0,
+            commentCount: 0
+        }
+
+        posts = [
+            ...posts,
+            post
+        ];
+
+        return post;
+    }
+
+    updatePost(postId: number, author: string, title: string, content: string) {
+        let findPost = posts.find((post) => post.id === postId);
+        if (!findPost) {
+            throw new NotFoundException();
+        }
+
+        if (author) {
+            findPost.author = author;
+        }
+
+        if (title) {
+            findPost.title = title;
+        }
+
+        if (content) {
+            findPost.content = content;
+        }
+
+        return findPost;
+    }
+
+    deletePost(id:number) {
+        const post = posts.find((post) => post.id === id);
+
+        if (!post) {
+            throw new NotFoundException();
+        }
+
+        posts = posts.filter(post => post.id !== id);
+        return id;
+    }
+}
